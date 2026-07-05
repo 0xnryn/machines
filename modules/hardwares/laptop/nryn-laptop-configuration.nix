6a0520710f949f0a1ce6e01b77abdcba01d47138
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-07-05 22:35:21 +0530

    update

--- modules/hardwares/laptop/nryn-laptop-configuration.nix
+++ modules/hardwares/laptop/nryn-laptop-configuration.nix
@@ -68,6 +68,12 @@
       networkmanager.enable = true;
       firewall.enable = true;
     };
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
   };
 }
 
